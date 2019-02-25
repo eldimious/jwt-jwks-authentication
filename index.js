3e6c610e3f70aef619287bd7d19ba6f3d617cb61
DimosthenisBotsaris <botsaris.d@gmail.com>
2019-02-25 14:00:05 +0200

    validate config

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const authenticationServiceModule = require('./domain/authenticationService');
 
-module.exports = function init(opts) {
-  // validate opts.jwksUri
-  const authenticationService = authenticationServiceModule.init(opts);
-  return {
+const validateConfiguration = function validateConfiguration(config) {
+  if (!config || !config.jwksUri) {
+    throw new Error('Add configuration object included jwksUri.');
+  }
+};
+
+module.exports = function init(config) {
+  try {
+    validateConfiguration(config);
+  } catch (error) {
+    throw error;
+  }
+  const authenticationService = authenticationServiceModule.init(config);
+  return Object.freeze({
     checkAuth: authenticationService.checkAuth,
-  };
+  });
 };
