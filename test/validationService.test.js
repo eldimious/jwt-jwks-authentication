faf72391d5978732eaccb37bae4bab339562c487
DimosthenisBotsaris <botsaris.d@gmail.com>
2019-02-26 10:00:35 +0200

    improve tests

--- test/validationService.test.js
+++ test/validationService.test.js
@@ -3,12 +3,18 @@ const {
   validateConfiguration,
 } = require('../src/domain/validationService');
 
-describe('validation service tests', function () {
-  describe('test validateConfiguration method', function () {
-    it('should return without any error', async function () {
+describe('validation service tests', () => {
+  describe('test validateConfiguration method', () => {
+    it('should return without any error, as we pass jwksUri', async () => {
       const fn = validateConfiguration({
         jwksUri: 'https://test.com',
-      })
+      });
+      expect(fn).to.be.undefined;
+    });
+    it('should return without any error, as we pass secret', async () => {
+      const fn = validateConfiguration({
+        secret: 'test',
+      });
       expect(fn).to.be.undefined;
     });
   });
