03562703c34a437a2b73480bcf3ccca418b87eb9
DimosthenisBotsaris <botsaris.d@gmail.com>
2019-03-03 12:26:45 +0200

    add tests for exported module

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,16 @@
 const { expect } = require('chai');
+const jwt = require('jsonwebtoken');
 const authenticationFactory = require('../src');
+
+const userEmail = 'test@gmail.com';
+const userName = 'Dimos';
+const userId = 1;
+const jwtSecret = 'secret';
+const token = jwt.sign({ email: 'test@gmail.com', name: 'Dimos', id: 1 }, jwtSecret);
 
 describe('authentication module tests', () => {
   describe('test exported module', () => {
-    it('should return method', async () => {
+    it('should return checkAuth method', async () => {
       const authentication = authenticationFactory({
         secret: 'secret',
       });
@@ -14,6 +21,26 @@ describe('authentication module tests', () => {
     it('should return validation error', async () => {
       const err = 'Should pass either a public key from jwks (secret) or jwks-rsa configuration (jwksUri) configuration option to decode incoming JWT token.';
       expect(authenticationFactory).to.throw(err);
+    });
+
+    describe('test exported function', () => {
+      it('should return checkAuth method', async () => {
+        const authentication = authenticationFactory({
+          secret: 'secret',
+        });
+        const req = {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        };
+        expect(typeof(authentication.checkAuth)).to.eql('function');
+        const decodedToken = await authentication.checkAuth(req);
+        expect(decodedToken).to.not.be.undefined;
+        expect(decodedToken).to.be.an('object');
+        expect(decodedToken.email).to.equal(userEmail);
+        expect(decodedToken.name).to.equal(userName);
+        expect(decodedToken.id).to.equal(userId);
+      });
     });
   });
 });
