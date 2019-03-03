11cded142a5be1d6c0f86e62b3027cd36b95a484
DimosthenisBotsaris <botsaris.d@gmail.com>
2019-03-03 16:15:14 +0200

    improve tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,51 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const jwt = require('jsonwebtoken');
+const { spy, stub } = require('sinon');
 const authenticationFactory = require('../src');
-const { mockRequest, mockResponse } = require('./mockData');
+
+
+const mockRequest = (options = {}) => ({
+  body: {},
+  cookies: {},
+  query: {},
+  params: {},
+  get: stub(),
+  ...options,
+});
+
+const mockResponse = (options = {}) => {
+  const res = {
+    cookie: spy(),
+    clearCookie: spy(),
+    download: spy(),
+    format: spy(),
+    json: spy(),
+    jsonp: spy(),
+    message: null,
+    send(msg) {
+      this.message = msg;
+      return this;
+    },
+    sendFile: spy(),
+    sendStatus: spy(),
+    redirect: spy(),
+    render: spy(),
+    end: spy(),
+    set: spy(),
+    type: spy(),
+    get: stub(),
+    code: null,
+    status(status) {
+      this.code = status;
+      return this;
+    },
+    ...options,
+
+  };
+  res.vary = stub().returns(res);
+  return res;
+};
 
 chai.use(chaiAsPromised);
 const {
