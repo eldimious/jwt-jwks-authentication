e298227f20c35b45b87e112ecef82b0107b29c89
DimosthenisBotsaris <botsaris.d@gmail.com>
2019-03-03 15:32:56 +0200

    Add more tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,8 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
-
 const jwt = require('jsonwebtoken');
 const authenticationFactory = require('../src');
+const { mockRequest, mockResponse } = require('./mockData');
 
 chai.use(chaiAsPromised);
 const {
@@ -14,8 +14,14 @@ const userName = 'Dimos';
 const userId = 1;
 const jwtSecret = 'secret';
 const token = jwt.sign({ email: 'test@gmail.com', name: 'Dimos', id: 1 }, jwtSecret);
+let req;
+let res;
 
 describe('authentication module tests', () => {
+  beforeEach(() => {
+    req = mockRequest();
+    res = mockResponse();
+  });
   describe('test exported module', () => {
     it('should return verify method', async () => {
       const authentication = authenticationFactory({
@@ -24,6 +30,7 @@ describe('authentication module tests', () => {
       expect(authentication).to.not.be.undefined;
       expect(authentication).to.be.an('object');
       expect(typeof(authentication.verify)).to.eql('function');
+      expect(typeof(authentication.authorize)).to.eql('function');
     });
     it('should return validation error', async () => {
       const err = 'Should pass either a public key from jwks (secret) or jwks-rsa configuration (jwksUri) configuration option to decode incoming JWT token.';
@@ -34,7 +41,7 @@ describe('authentication module tests', () => {
         const authentication = authenticationFactory({
           secret: 'secret',
         });
-        const req = {
+        req = {
           headers: {
             authorization: `Bearer ${token}`,
           },
@@ -51,12 +58,63 @@ describe('authentication module tests', () => {
         const authentication = authenticationFactory({
           secret: 'secret',
         });
-        const req = {
+        req = {
           headers: {
           },
         };
         await expect(authentication.verify(req)).to.eventually.be.rejectedWith(Error);
       });
+    });
+    describe('test authorize function', () => {
+      it('should return error, not passing res', async () => {
+        const authentication = authenticationFactory({
+          secret: 'secret',
+        });
+        req = {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        };
+        await expect(authentication.authorize(req)).to.eventually.be.rejectedWith(Error);
+      });
+      it('should return error, not passing res', async () => {
+        const authentication = authenticationFactory({
+          secret: 'secret',
+        });
+        req = {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        };
+        await expect(authentication.authorize(req)).to.eventually.be.rejectedWith(Error);
+      });
+      // it('should return token without error', async () => {
+      //   const authentication = authenticationFactory({
+      //     secret: 'secret',
+      //   });
+      //   const req = {
+      //     headers: {
+      //       authorization: `Bearer ${token}`,
+      //     },
+      //   };
+      //   expect(typeof(authentication.verify)).to.eql('function');
+      //   const decodedToken = await authentication.verify(req);
+      //   expect(decodedToken).to.not.be.undefined;
+      //   expect(decodedToken).to.be.an('object');
+      //   expect(decodedToken.email).to.equal(userEmail);
+      //   expect(decodedToken.name).to.equal(userName);
+      //   expect(decodedToken.id).to.equal(userId);
+      // });
+      // it('should return error', async () => {
+      //   const authentication = authenticationFactory({
+      //     secret: 'secret',
+      //   });
+      //   const req = {
+      //     headers: {
+      //     },
+      //   };
+      //   await expect(authentication.verify(req)).to.eventually.be.rejectedWith(Error);
+      // });
     });
   });
 });
