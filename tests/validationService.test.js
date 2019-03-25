a1f00249ca1fca6d3ee40ee9efc622570b73a7ea
DimosthenisBotsaris <botsaris.d@gmail.com>
2019-03-25 15:56:12 +0200

    Adjust tests

--- tests/validationService.test.js
+++ tests/validationService.test.js
@@ -1,27 +1,27 @@
 const { expect } = require('chai');
 const {
-  validateConfiguration,
+  validateConfigurationParam,
 } = require('../src/domain/validationService');
 
 describe('validation service tests', () => {
-  describe('test validateConfiguration method', () => {
+  describe('test validateConfigurationParam method', () => {
     it('should return without any error, as we pass jwksUri', async () => {
-      const fn = validateConfiguration({
+      const fn = validateConfigurationParam({
         jwksUri: 'https://test.com',
       });
       expect(fn).to.be.undefined;
     });
     it('should return without any error, as we pass secret', async () => {
-      const fn = validateConfiguration({
+      const fn = validateConfigurationParam({
         secret: 'test',
       });
       expect(fn).to.be.undefined;
     });
     it('should return throw error, bad config', async () => {
-      const fn = validateConfiguration({
+      const fn = validateConfigurationParam({
         secret: 'test',
       });
-      expect(validateConfiguration).to.throw('Should pass either a public key from jwks (secret) or jwks-rsa configuration (jwksUri) configuration option to decode incoming JWT token.');
+      expect(validateConfigurationParam).to.throw('Should pass either a public key from jwks (secret) or jwks-rsa configuration (jwksUri) configuration option to decode incoming JWT token.');
     });
   });
 });
